6f7e3ec9688c91e60f705c8a806d71e704518cab
pongloongyeat <pongloongyeat@gmail.com>
2021-09-01 23:56:36 +0800

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'date'
+
+10.times do |i|
+  Todo.create(
+    title: "Todo #{i + 1}",
+    description: "#{i + 1} Lorem ipsum saves lives",
+    is_done: rand(2) == 1,
+    due_date: (rand(2) == 1) ? nil : DateTime.now()
+  )
+end
